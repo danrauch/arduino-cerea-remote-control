55eea32ec5926e6354f70addde78929053be93bc
Daniel Rauch <dan.rauch@yahoo.de>
2019-03-14 21:07:56 +0100

    use correct encoding in the simulator

--- util/cerea_simulator.py
+++ util/cerea_simulator.py
@@ -2,25 +2,28 @@ import sys
 import time
 import serial
 
+
 def main():
     com_port = 'COM6'
-
+    send_delay = 0.50
     try:
-        serial_port = serial.Serial(com_port, timeout=0.1)        
+        serial_port = serial.Serial(com_port, baudrate=115200, timeout=0.010)
     except serial.SerialException as e:
         print(e)
         sys.exit(1)
-    
+
     try:
-        while True:    
-            serial_port.write("@CEREA;5.0;-1;1;1;1;1;1;1;1;1;1;1;END")
-            time.sleep(0.5)
-            serial_port.write("@HIDRAU;1;END")
+        while True:
+            print("send")
+            serial_port.write(b'@CEREA;5.0;-1;1;1;1;1;1;1;1;1;1;1;END\r\n')
+            time.sleep(send_delay)
             print(serial_port.read_until())
-            time.sleep(0.5)            
+            serial_port.write(b'@HIDRAU;1;END\r\n')
+            time.sleep(send_delay)
+            print(serial_port.read_until())
     except KeyboardInterrupt:
         print('Keyboard interrupt by user - exit')
-        sys.exit(1) 
+        sys.exit(1)
 
 
 if __name__ == '__main__':
